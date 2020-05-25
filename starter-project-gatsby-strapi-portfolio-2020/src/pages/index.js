ddc58ebde5a39ada92f1769a020a2cb338a71044
jason loomis <jason@jrloomis.com>
2020-05-25 12:50:36 -0400

    projects - section & page

--- starter-project-gatsby-strapi-portfolio-2020/src/pages/index.js
+++ starter-project-gatsby-strapi-portfolio-2020/src/pages/index.js
@@ -7,13 +7,44 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
-export default () => {
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        strapiId
+        featured
+        github
+        id
+        title
+        url
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
--- starter-project-gatsby-strapi-portfolio-2020/src/pages/projects.js
+++ starter-project-gatsby-strapi-portfolio-2020/src/pages/projects.js
@@ -2,10 +2,46 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects=page">
+        <Projects projects={projects} title="all project" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        strapiId
+        featured
+        github
+        id
+        title
+        url
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
